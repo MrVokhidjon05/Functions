ff331496a2669dda662b96e2cf0409927e4f81ff
Vokhidjon Zokirov <vokhidjonzokirov@Vokhidjons-MacBook-Air.local>
2024-12-28 20:47:43 +0900

    First

---
+++ index.js
@@ -0,0 +1,353 @@
+// let weather = `issi`;
+// let age = `25`;
+// switch (weather) {
+//     case `iliq`:
+//          console.log(`bugun havo iliq`);
+//          break;
+//     case `sovuq` :
+//          console.log(`bugun havo sovuq`);
+//          break;
+//     case  `issiq`: 
+//          console.log(`bugun havo issiq`);
+//          break;
+//     default:
+//         console.log(`bunday malumot mavjud emas`);
+// }
+
+
+// function tessen(){
+//     console.log(`qwerty`);
+
+// }
+
+// for(let i=0; i<=5; i++) {
+//     tessen()
+// }
+
+
+
+
+// .  1 Write a function that takes a number (1-7) and logs the corresponding day of the week.
+// function findTheDay(day) {
+//     switch (day) {
+//         case 1:
+//             console.log('Monday');
+//             break;
+//         case 2:
+//             console.log('Tuesday');
+//             break;
+//         case 3:
+//             console.log('Wednesday');
+//             break;
+//         case 4:
+//             console.log('Thursday');
+//             break;
+//         case 5:
+//             console.log('Friday');
+//             break;
+//         case 6:
+//             console.log('Saturday');
+//             break;
+//         case 7:
+//             console.log('Sunday');
+//             break;
+//         default:
+//             console.log('Invalid day');
+//     }
+// }
+
+// findTheDay(5)
+
+
+/// .   2 Write a function that takes a browser name and logs a message indicating support (e.g., 'Chrome', 'Firefox', 'Safari').
+
+// function BrowserSupport(browse) {
+//     switch (browse) {
+//         case "Chrome":
+//             console.log("Browse support Chrome");
+//             break;
+//         case "Fox":
+//             console.log("Browse support Fox");
+//             break;
+//         case "Google":
+//             console.log("Browse support Google");
+//             break;
+//         case "Safari":
+//             console.log("Browse support Safari");
+//             break;
+//         default:
+//             console.log("Invalid web-page");
+//     }
+// }
+// BrowserSupport("Safari")
+
+
+/// .   3 Write a function that takes a shape name ('circle', 'square', 'triangle') and logs the number of sides or properties of the shape.
+
+// function propertiesoftheshape(shape) {
+//     switch (shape) {
+//         case `circle`:
+//             console.log('No side');
+//             break;
+//         case `square`:
+//             console.log('Four sides');
+//             break;
+//         case `triangle`:
+//             console.log('Three sides');
+//             break;
+//         default:
+//             console.log('Unknown shape');
+//     }
+// }
+
+// propertiesoftheshape(`square`)
+
+
+///  .  4 Write a function that takes a user role ('admin', 'editor', 'viewer') and logs the corresponding permissions.
+// function UserRole(role) {
+//     switch (role) {
+//         case `admin`:
+//             console.log('Full access');
+//             break;
+//         case `editor`:
+//             console.log('Edit access');
+//             break;
+//         case `viewer`:
+//             console.log('View access');
+//             break;
+//         default:
+//             console.log('Invalid role');
+//     }
+// }
+
+// UserRole(`admin`)
+
+///  .  5 Write a function that takes a letter grade ('A', 'B', 'C', 'D', 'F') and logs a message indicating the performance level.
+// function LetterGrade(grade) {
+//     switch (grade) {
+//         case `A`:
+//             console.log(`Exelent`);
+//             break
+//         case `B`:
+//             console.log(`Good`);
+//             break
+//         case `C`:
+//             console.log(`Normal`);
+//             break
+//         case `D`:
+//             console.log(`Bad`);
+//             break
+//         case `F`:
+//             console.log(`Fail`);
+//             break
+//         default:
+//             console.log(`Invalid Grade`);
+
+//     }
+// }
+// LetterGrade(`D`)
+
+
+/// .   6 Write a function that takes an animal type ('dog', 'cat', 'cow', etc.) and logs the corresponding domestic or wild.
+// function AnimalType(type) {
+//     switch (type) {
+//         case 'dog':
+//             console.log('domestic');
+//             break
+//         case 'cat':
+//             console.log('domestic');
+//             break
+//         case 'wolf':
+//             console.log('wild');
+//             break
+//         case 'bear':
+//             console.log('wild');
+//             break
+//         default:
+//             console.log('Unknown type of animal');
+//     }
+// }
+// AnimalType('wolf')
+
+
+/// .   7 Write a function that takes a menu option (1-5) and logs the corresponding fast food's name.
+
+// function fastfood(menu) {
+//     switch (menu) {
+//         case 1:
+//             console.log('Hotdog');
+//             break
+//         case 2:
+//             console.log('Humbuger');
+//             break
+//         case 3:
+//             console.log('Pizza');
+//             break
+//         case 4:
+//             console.log('Lavash');
+//             break
+//         case 5:
+//             console.log('Donar');
+//             break
+//         default:
+//             console.log('We do not have in our menu');
+//     }
+// } 
+// fastfood(6)
+
+
+///     .   8 Write a function that takes a traffic light color ('red', 'yellow', 'green') and logs the corresponding action (e.g., 'stop', 'caution', 'go').
+
+// function TrafficLight(color) {
+//     switch (color) {
+//         case 'red':
+//             console.log('stop');
+//             break;
+//         case 'yellow':
+//             console.log('caution');
+//             break;
+//         case 'green':
+//             console.log('go');
+//             break;
+//         default:
+//             console.log('Invalid color');
+//     }
+// }
+// TrafficLight('white')
+
+
+///     .   9 Write a function that takes two numbers and an operator ('+', '-', '*', '/') and logs the result of the operation.
+// function Calculator(operator) {
+//     switch (operator) {
+//         case '+':
+//             console.log(20 + 10);
+//             break;
+//         case '-':
+//             console.log(40 - 20);
+//             break;
+//         case '*':
+//             console.log(4 * 40);
+//             break;
+//         case '/':
+//             console.log(50 / 2);
+//             break;
+//         default:
+//             console.log('Invalid operator');
+//     }
+// } 
+// Calculator('*')
+
+
+///     .   10  Write a function that takes a month number (1-12) and logs the corresponding season ('Winter', 'Spring', 'Summer', 'Autumn').
+
+// function CorrespondingSeason(month){
+//     switch(month){
+//         case 1:
+//         case 2:
+//         case 12:
+//             console.log('Winter');
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             console.log('Spring');
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             console.log('Summer');
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             console.log('Autumn')
+//             break;
+//         default:
+//             console.log('Unknown season');    
+//     }
+// }
+// CorrespondingSeason(9)
+
+
+
+///     .   11
+// let car = 5;
+// while(car >3){
+//     console.log(`Oka pulingiz yetmaydi ${car}`);
+//     break;
+// }
+
+
+///     .   12
+// let book = 32;
+// while(book < 60){
+//     console.log(`O'qish kerak ${book}ta kitob`);
+//     break;
+//     }
+
+
+///     .   13
+// let xotin = 4
+// while(xotin >1){
+//     console.log(`${xotin}ta kam yana uylaning`);
+//     break;
+    
+// }
+
+
+///     .   14
+// let phone = 2
+// while(phone >1){
+//     console.log(`${phone}ta teldan ko'ra bitta komp. yaxshi`);
+//     break;
+    
+// }
+
+
+///     .   15
+// let degree = -5
+
+// while (degree < -1) {
+//   console.log(`${degree} gradusda suv muzlaydi`); 
+//   break;
+// }
+
+
+///     .   16
+// let number = 1;
+
+// do {
+//   console.log(number); 
+//   number++;          
+// } while (number <= 5);
+
+
+///     .   17
+// let number = 10;
+// do {
+//     console.log(number);
+//     number--;
+// } while (number >= 0);
+
+
+///     .   18
+// let n = 3;
+// do {
+//     console.log(n);
+//     n += 2;   
+// }while( n <=20)
+
+
+///     .   19
+// let sleeping = 38;
+// do {
+//     console.log(`Uyqi kam, yana uxla`);
+//  break
+// }while(sleeping < 6)
+   
+
+///     .   20 
+// let language = 3
+// do {
+//     console.log(`Ko'proq til o'rganing`);
+// }while(language < 3)
